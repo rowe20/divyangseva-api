7dc4b02bf8ae5049b40e113222888766c34c0464
mr-sudheeshkumar <sudhikumar8055@gmail.com>
2022-04-26 12:38:22 +0530

    update

--- divyangseva/src/main/java/com/sk/divyangseva/repository/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sk.divyangseva.repository;
-
-import com.sk.divyangseva.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City,Long> {
-}
--- divyangseva/src/main/java/com/sk/divyangseva/repository/DisabilityCertificatesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sk.divyangseva.repository;
-
-import com.sk.divyangseva.model.DisabilityCertificates;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DisabilityCertificatesRepository extends JpaRepository<DisabilityCertificates,Long> {
-}
